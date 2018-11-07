6b33e88e65b44fa10160c84b3a7a1fb58fa88594
iseya <isyknj1999@gmail.com>
2018-11-07 22:40:05 +0900

    例外をthrowできるrunという関数の作成とmain関数で例外をcatchするように

--- program/src/main.cpp
+++ program/src/main.cpp
@@ -1,9 +1,28 @@
 #include "BeagleBone_Black.hpp"
 #include <iostream>
+#include <exception>
+
+namespace BBB{
+	//無限ループされながら実行される関数
+	void run(void);
+};
+
+
+void BBB::run(void){
+	
+	
+}
+
 
 int main(void){
 
-    std::cout << "Hello, World\n";
+    try {
+		while(true) BBB::run();
+	}
+	catch(std::exception& e){
+		std::cout << e.what() << std::endl;
+	}
+	catch(...){ }
 
     return 0;
 }
