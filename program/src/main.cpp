acbe0adeec594e260d09de55240f825dffe9fc33
iseya <isyknj1999@gmail.com>
2018-11-29 22:10:12 +0900

    モーター動作確認用のプログラム作成

--- program/src/main.cpp
+++ program/src/main.cpp
@@ -1,23 +1,48 @@
 #include "BeagleBone_Black.hpp"
 #include <iostream>
 #include <exception>
+#include <chrono>
 
 namespace BBB{
-	//無限ループされながら実行される関数
 	void run(void);
 };
 
 
-void BBB::run(void){
-	
-	
+void BBB::run(void)
+{
+	auto pwm1 = BBB::PWM(14, 15);
+	auto in1 = BBB::GPIO(61), in2 = BBB::GPIO(60);
+
+	in1.setDirection(false);
+	in2.setDirection(false);
+
+	in1.setValue(true);
+	in2.setValue(false);
+
+	pwm1.setPeriod(10000000);
+	pwm1.setDuty(pwm1.getDuty() * 0.5);
+
+	pwm1.run();
+
+	const int RUNTIME = 10000;
+
+	auto start = std::chrono::system_clock::now();
+	while(true)
+	{
+		auto nowTime =  std::chrono::system_clock::now();
+		auto dTime = nowTime - start;
+		auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(dTime).count();
+		if(msec > RUNTIME) break;
+	}
+
+	pwm1.stop();
 }
 
 
-int main(void){
-
+int main(void)
+{
     try {
-		while(true) BBB::run();
+		BBB::run();
 	}
 	catch(std::exception& e){
 		std::cout << e.what() << std::endl;
