3ca0613f4ff2c387c75922c57fd0b1ec1b91d477
iseya <isyknj1999@gmail.com>
2018-11-29 22:08:23 +0900

    正常にバイナリを読み書きできるように改良

--- program/src/BBB_pwm.cpp
+++ program/src/BBB_pwm.cpp
@@ -14,8 +14,8 @@ PWM::PWM(int capemgr, int ocp, int pinConnector, int pinNum, int pwmNum)
     pwmFile << "am33xx_pwm";
     pwmFile.close();
 
-    pwmFile = ofstream(path.str());
-    pwmFile << "bone_pwm_P" << pinConnector << "_" << pinNum;
+    ofstream pwmFile2(path.str());
+    pwmFile2 << "bone_pwm_P" << pinConnector << "_" << pinNum;
 
     path << "/sys/devices/ocp." << ocp << "pwm_test_P" 
         << pinConnector << "_" << pinNum << "." << pwmNum;
@@ -25,38 +25,40 @@ PWM::PWM(int capemgr, int ocp, int pinConnector, int pinNum, int pwmNum)
 void PWM::setPeriod(int period)
 {
     ofstream file(PWMPath + "/period", ios::binary);
-    file.write((char *)&period, sizeof(period));
+    file.write(to_string(period).c_str(), sizeof(int));
 }
 
 void PWM::setDuty(int duty)
 {
     ofstream file(PWMPath + "/duty", ios::binary);
-    file.write((char *)&duty, sizeof(duty));
+    file.write(to_string(duty).c_str(), sizeof(int));
 }
 
 void PWM::setPolarity(bool polarity)
 {
     ofstream file(PWMPath + "/polarity", ios::binary);
-    file.write((char *)&polarity, sizeof(polarity));
+
+    if(polarity) file.write("1", sizeof(int));
+    else         file.write("0", sizeof(int));
 }
 
 void PWM::run()
 {
     ofstream file(PWMPath + "/run", ios::binary);
-    file.write((char *)1, sizeof(int));
+    file.write("1", sizeof("1"));
 }
 
 void PWM::stop()
 {
     ofstream file(PWMPath + "/run", ios::binary);
-    file.write((char *)0, sizeof(int));
+    file.write("0", sizeof("0"));
 }
 
 int PWM::getPeriod()
 {
     ifstream file(PWMPath + "/period", ios::binary);
     int period;
-    file.read((char *)&period, sizeof(period));
+    file.read(reinterpret_cast<char*>(&period), sizeof(int));
     return period;
 }
 
@@ -64,7 +66,7 @@ int PWM::getDuty()
 {
     fstream file(PWMPath + "/duty", ios::binary);
     int duty;
-    file.read((char *)&duty, sizeof(duty));
+    file.read(reinterpret_cast<char*>(&duty), sizeof(duty));
     return duty;
 }
 
@@ -72,20 +74,18 @@ bool PWM::getPolarity()
 {
     ifstream file(PWMPath + "/polarity", ios::binary);
     int tmp;
-    file.read((char *)&tmp, sizeof(tmp));
-    if (tmp == 0)
-        return 0;
-    else
-        return 1;
+    file.read(reinterpret_cast<char*>(&tmp), sizeof(tmp));
+    
+    if (tmp == 0) return 0;
+    else          return 1;
 }
 
-bool PWM::getIsRun()
+bool PWM::isRun()
 {
     ifstream file(PWMPath + "/run", ios::binary);
     int tmp;
-    file.read((char *)&tmp, sizeof(tmp));
-    if (tmp == 1)
-        return true;
-    else
-        return false;
+    file.read(reinterpret_cast<char*>(&tmp), sizeof(tmp));
+
+    if (tmp == 1) return true;
+    else          return false;
 }
