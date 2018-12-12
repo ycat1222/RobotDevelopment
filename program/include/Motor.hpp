5834a02a3aa45839b45fa5e979f7160a3547b6a5
Iseya <Kenji-Iseya@users.noreply.github.com>
2018-12-12 23:23:48 +0900

    モータに関するクラスの追加

--- program/include/Motor.hpp
+++ program/include/Motor.hpp
@@ -4,13 +4,26 @@
 namespace BBB{
 class Motor{
 public:
-    void setPWM(int pinNum);
+	explicit Motor(int pwmPinNum, int gpioNumIN1, int gpioNumIN2);
+
+    void setPWM(int pwmPinNum);
     void setGPIO(int gpioNumIN1, int gpioNumIN2);
-    void runNormalRotation();
-    void runReverseRotation();
+
+	// IN1 = 1, IN2 = 0 に設定
+    void runNormal();
+	// IN1 = 0, IN2 = 1 に設定
+    void runReverse();
+	// IN1 = IN2 = 1 に設定
+	void brake();
+	// IN1 = IN2 = 0 に設定
+	void stop();
     
 private:
+	explicit Motor();
+
     PWM pwm;
     GPIO IN1, IN2;
+
+	static int pwmUniqueNum;
 };
 };
--- program/src/Motor.cpp
+++ program/src/Motor.cpp
@@ -1,12 +1,66 @@
 #include<Motor.hpp>
 using namespace BBB;
 
-void Motor::setPWM(int pinNum)
-{
+// PWM固有の値(静的変数)の初期化
+int BBB::Motor::pwmUniqueNum = 15;
 
+
+BBB::Motor::Motor(int pwmPinNum, int gpioNumIN1, int gpioNumIN2)
+{
+	pwm = PWM(pwmPinNum, pwmUniqueNum);
+	IN1 = GPIO(gpioNumIN1);
+	IN2 = GPIO(gpioNumIN2);
+
+	//たぶん有効化するごとに、1ずつ増えてく?
+	pwmUniqueNum++;
+}
+
+void Motor::setPWM(int pwmPinNum)
+{
+	pwm = PWM(pwmPinNum, pwmUniqueNum);
 }
 
 void Motor::setGPIO(int gpioNumIN1, int gpioNumIN2)
 {
+	IN1 = GPIO(gpioNumIN1);
+	IN2 = GPIO(gpioNumIN2);
+}
 
+void BBB::Motor::setDutyRate(double rate)
+{
+	pwm.setDutyRate(rate);
+}
+
+void BBB::Motor::dutyRate()
+{
+	return pwm.dutyRate();
+}
+
+void BBB::Motor::duty()
+{
+	return pwm.duty();
+}
+
+void BBB::Motor::runNormal()
+{
+	IN1.setValue(1);
+	IN2.setValue(0);
+}
+
+void BBB::Motor::runReverse()
+{
+	IN1.setValue(0);
+	IN2.setValue(1);
+}
+
+void BBB::Motor::brake()
+{
+	IN1.setValue(1);
+	IN2.setValue(1);
+}
+
+void BBB::Motor::stop()
+{
+	IN1.setValue(0);
+	IN2.setValue(0);
 }
