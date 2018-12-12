24293d5cfefd5c6123e8a0c78eb290271b8270e9
Iseya <Kenji-Iseya@users.noreply.github.com>
2018-12-12 23:15:16 +0900

    コンストラクタの引数をchar*にして、what() をちゃんとオーバーライドするように設定

--- program/include/BBB_error.hpp
+++ program/include/BBB_error.hpp
@@ -2,13 +2,17 @@
 #include <exception>
 #include <string>
 
-namespace BBB{
-class ErrorBBB : public std::exception{ 
-  private:
-  std::string str;
+namespace BBB {
+	class ErrorBBB : public std::exception {
+	private:
+		std::string str;
 
-  public:
-  ErrorBBB(std::string str_) : str(str_) {};
-  const char* what() {return str.c_str();}
-};
+	public:
+		ErrorBBB(char* str_)
+			{ str = str_; }
+
+		const char* what() const override
+			{ return str.c_str(); }
+
+	};
 };
