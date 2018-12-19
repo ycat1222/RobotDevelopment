88d38948f0ed815a84df58732e383a9ccae2855f
Iseya <Kenji-Iseya@users.noreply.github.com>
2018-12-19 23:00:22 +0900

    ・コンストラクタを継承して、GPIOを入力に設定 ・時間→距離 の変換係数の修正

--- program/include/UltraSonicSensor.hpp
+++ program/include/UltraSonicSensor.hpp
@@ -3,10 +3,17 @@
 
 class UltraSonicSensor : public BBB::GPIO {
 public:
+
+	UltraSonicSensor() {};
+	UltraSonicSensor(int gpioNum_) : BBB::GPIO(gpioNum_)
+	{
+		this->setDirection(true);
+	}
+
 	//距離[mm]を返す
 	double distance()
 	{
-		// 2*距離d÷時間t = 音速V → d = 0.5*V*t = 0.6874[mm/μs]*t[μs]
-		return this->value()*0.6874;
+		// 2*距離d÷時間t = 音速V → d = 0.5*V*t = 0.1718[mm/μs]*t[μs]
+		return this->value()*0.1718;
 	}
 };
