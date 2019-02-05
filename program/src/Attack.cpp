758909b9217a0b54152100cb3a5b7a82442b354f
iseya <isyknj1999@gmail.com>
2019-02-05 22:22:36 +0900

    攻撃側のいろいろを修正 ・オブジェクトを一度生成だけ生成するように ・初期化が不十分なところをちゃんと初期化 ・autoをint型へ変更(x-1とかしたときに、オーバーフローしていたため) ・壁があるマスに突っ込まないように変更 ・passedMapをちゃんと更新 ・方向が1箇所間違っていたので修正 ・敵の判定を近くにいた場合に限定

--- program/src/Attack.cpp
+++ program/src/Attack.cpp
@@ -11,11 +11,6 @@ void BBB::attack()
 
 	Map map("field.txt");
 
-	//通過済みのマスの情報が入った配列(初期ではfalse)
-	std::vector<std::vector<bool>> passedMap(Map::MAP_SIZE, std::vector<bool>(Map::MAP_SIZE, false));
-
-	passedMap[0][0] = true;
-
 	enum Direction
 	{
 		east = 0,
@@ -24,32 +19,54 @@ void BBB::attack()
 		north = 3,
 	};
 
+	//通過済みのマスの情報が入った配列(初期ではfalse)
+	std::vector<std::vector<bool>> passedMap(Map::MAP_SIZE, std::vector<bool>(Map::MAP_SIZE, false));
+
+	//それぞれの方向の壁がないマスの数
+	std::array<unsigned int, 4> numBlankCell = { 0,0,0,0 };
+
+	//壁に阻まれず、かつ、ボーナスマスがあるときの方向
+	std::array<bool, 4> bounusDirection = { false, false, false, false };
+
+	// 敵がいる方向
+	std::array<bool, 4> entityDirection = { false, false, false, false };
+
+	//それぞれの方向の得点した壁までのマスの数
+	std::array<int, 4> numPassedCell = { 0,0,0,0 };
+
 	while (true) {
 
 		unsigned int x = robot.x() / Map::CELL_SIZE;
 		unsigned int y = robot.y() / Map::CELL_SIZE;
 
-		//それぞれの方向の壁がないマスの数
-		std::array<int, 4> numBlankCell;
-		for (auto xi = x + 1; xi < Map::MAP_SIZE; xi++)
+		numBlankCell = { 0,0,0,0 };
+		bounusDirection = { false, false, false, false };
+		entityDirection = { false, false, false, false };
+		numPassedCell = { 0,0,0,0 };
+
+		std::cout << robot.x() << ", " << robot.y() << std::endl;
+
+		passedMap[x][y] = true;
+
+		//それぞれの方向の壁がないマスの数を取得
+		for (int xi = x + 1; xi < Map::MAP_SIZE; xi++)
 			if (map[xi][y].east == 0) numBlankCell[east]++;
 
-		for (auto xi = x - 1; xi > 0; xi--)
-			if (map[xi][y].east == 0) numBlankCell[west]++;
+		for (int xi = x - 1; xi > 0; xi--)
+			if (map[xi][y].west == 0) numBlankCell[west]++;
 
-		for (auto yi = y + 1; yi < Map::MAP_SIZE; yi++)
+		for (int yi = y + 1; yi < Map::MAP_SIZE; yi++)
 			if (map[x][yi].north == 0) numBlankCell[north]++;
 
-		for (auto yi = y - 1; yi > 0; yi--)
+		for (int yi = y - 1; yi > 0; yi--)
 			if (map[x][yi].south == 0) numBlankCell[south]++;
 
 		//壁がない=まっすぐ進めるマスが一番多い方向を取得
 		auto maxItr = std::max_element(numBlankCell.begin(), numBlankCell.end());
 		auto maxBlankDirection = std::distance(numBlankCell.begin(), maxItr);
 
-		//壁に阻まれず、かつ、ボーナスマスがあるときの方向を取得
-		std::array<bool, 4> bounusDirection = { false, false, false, false };
 
+		//壁に阻まれず、かつ、ボーナスマスがあるときの方向を取得
 		if (map[x][y].east == false && map[x + 1][y].isBonus)
 			bounusDirection[east] = true;
 
@@ -62,34 +79,32 @@ void BBB::attack()
 		if (map[x][y].south == false && map[x][y - 1].isBonus)
 			bounusDirection[south] = true;
 
-		// 敵がいる方向の判定
-		std::array<bool, 4> entityDirection = { false, false, false, false };
 
-		if (std::abs(numBlankCell[east] * Map::CELL_SIZE - robot.distanceEast()) > Map::CELL_SIZE + 10.0)
+		// 敵がいる方向の判定 (その方向に壁がないのに、明らかに距離が近い場合)
+		if (map[x][y].east == false && robot.distanceEast() < Map::CELL_SIZE)
 			entityDirection[east] = true;
 
-		else if (std::abs(numBlankCell[west] * Map::CELL_SIZE - robot.distanceWest()) > Map::CELL_SIZE + 10.0)
+		else if (map[x][y].west == false && robot.distanceWest() < Map::CELL_SIZE)
 			entityDirection[west] = true;
 
-		else if (std::abs(numBlankCell[south] * Map::CELL_SIZE - robot.distanceSouth()) > Map::CELL_SIZE + 10.0)
-			entityDirection[south] = true;
-
-		else if (std::abs(numBlankCell[north] * Map::CELL_SIZE - robot.distanceNorth()) > Map::CELL_SIZE + 10.0)
+		else if (map[x][y].north == false && robot.distanceNorth() < Map::CELL_SIZE)
 			entityDirection[north] = true;
 
+		else if (map[x][y].south == false && robot.distanceSouth() < Map::CELL_SIZE)
+			entityDirection[south] = true;
 
-		//それぞれの方向の得点したマスの数(壁までの)
-		std::array<int, 4> numPassedCell;
-		for (auto xi = x + 1; xi < x + numBlankCell[east]; xi++)
+
+		//それぞれの方向の得点した壁までのマスの数を取得
+		for (int xi = x + 1; xi < x + numBlankCell[east]; xi++)
 			if (passedMap[xi][y]) numPassedCell[east]++;
 
-		for (auto xi = x - 1; xi > 0; xi--)
+		for (int xi = x - 1; xi > 0; xi--)
 			if (passedMap[xi][y]) numPassedCell[west]++;
 
-		for (auto yi = y + 1; yi < y + numBlankCell[north]; yi++)
+		for (int yi = y + 1; yi < y + numBlankCell[north]; yi++)
 			if (passedMap[x][yi]) numPassedCell[north]++;
 
-		for (auto yi = y - 1; yi > 0; yi--)
+		for (int yi = y - 1; yi > 0; yi--)
 			if (passedMap[x][yi]) numPassedCell[south]++;
 
 		// すでに通ったマスが一番少ない(得点を取れる)方向 を取得
@@ -109,37 +124,34 @@ void BBB::attack()
 
 		for (auto d = 0; d < 4; d++) {
 			//敵がいない方向で
-			if (!entityDirection[d]) {
+			if (entityDirection[d] == false) {
 				//ボーナスがあるなら、その方向へ
 				if (bounusDirection[d]) {
 					if (d == east) robot.moveEast(Map::CELL_SIZE);
 					if (d == west) robot.moveWest(Map::CELL_SIZE);
 					if (d == north) robot.moveNorth(Map::CELL_SIZE);
 					if (d == south) robot.moveSouth(Map::CELL_SIZE);
+					passedMap[x][y] = true;
 					continue;
 				}
-				//ボーナスがないなら、まだ通ってないマスへ
+
+				//その方向が壁ではなく、かつ、ボーナスがないなら、まだ通ってないマスへ
+				if (numBlankCell[east] > 0 && minPassedDirection == east) robot.moveEast(Map::CELL_SIZE);
+				else if (numBlankCell[west] > 0 && minPassedDirection == west) robot.moveWest(Map::CELL_SIZE);
+				else if (numBlankCell[north] > 0 && minPassedDirection == north) robot.moveNorth(Map::CELL_SIZE);
+				else if (numBlankCell[south] > 0 && minPassedDirection == south) robot.moveSouth(Map::CELL_SIZE);
 				else {
-					if		(minPassedDirection == east) robot.moveEast(Map::CELL_SIZE);
-					else if (minPassedDirection == west) robot.moveWest(Map::CELL_SIZE);
-					else if (minPassedDirection == north) robot.moveNorth(Map::CELL_SIZE);
-					else if (minPassedDirection == south) robot.moveSouth(Map::CELL_SIZE);
-					else {
-						//まだ通っていないマスがなければ、一番 壁までが長いとこへすすむ
-						if		(maxBlankDirection == east) robot.moveEast(Map::CELL_SIZE);
-						else if (maxBlankDirection == west) robot.moveWest(Map::CELL_SIZE);
-						else if (maxBlankDirection == north) robot.moveNorth(Map::CELL_SIZE);
-						else if (maxBlankDirection == south) robot.moveSouth(Map::CELL_SIZE);
-					}
-					continue;
-					
+					//まだ通っていないマスがなければ、一番 壁までが長いとこへすすむ
+					if (maxBlankDirection == east) robot.moveEast(Map::CELL_SIZE);
+					else if (maxBlankDirection == west) robot.moveWest(Map::CELL_SIZE);
+					else if (maxBlankDirection == north) robot.moveNorth(Map::CELL_SIZE);
+					else if (maxBlankDirection == south) robot.moveSouth(Map::CELL_SIZE);
 				}
+				passedMap[x][y] = true;
 			}
 		}
 
 	} //while終わり
 	
 
-
-	
 }
