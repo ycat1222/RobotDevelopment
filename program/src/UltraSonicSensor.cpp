0cc6a530a944b391ca7f4b7402fbdfa85b7a5d24
Iseya <Kenji-Iseya@users.noreply.github.com>
2019-02-05 23:13:07 +0900

    UltraSonicSensorクラスで、要らないコメント化の部分を削除

--- program/src/UltraSonicSensor.cpp
+++ program/src/UltraSonicSensor.cpp
@@ -19,38 +19,7 @@ double BBB::UltraSonicSensor::distance()
 	path << "/sys/class/gpio/gpio" << gpioNum << "/value";
 	ifstream valueFile(path.str());
 
-	//auto valueFd = open(path.str().c_str(), O_RDONLY);
-
-	//pollfd pfd;
-	//pfd.fd = valueFd;
-	//pfd.events = POLLPRI;
-
-	//auto edgeUpTime = chrono::high_resolution_clock::now();
-	//auto edgeDownTime = chrono::high_resolution_clock::now();
 	char c;
-
-	/*
-	while (true) {
-		lseek(valueFd, 0, SEEK_SET);
-		auto ret = poll(&pfd, 1, -1);
-		read(valueFd, &c, 1);
-		//パルスの立ち上がり時刻を取得
-		if (c == '1') edgeUpTime = chrono::high_resolution_clock::now();
-		else continue;
-
-		lseek(valueFd, 0, SEEK_SET);
-		ret = poll(&pfd, 1, -1);
-		read(valueFd, &c, 1);
-		//パルスの立ち下がり時刻を所得
-		if (c == '0') nowTime = chrono::high_resolution_clock::now();
-		else continue;
-
-		//立ち下がり - 立ち上がり の時間計算
-		auto dtime = edgeDownTime - edgeUpTime;
-		// 2*距離d÷時間t = 音速V → d[mm] = 0.5*V*t = 0.1718[mm/μs]*t[μs]
-		return 0.1717975*chrono::duration_cast<chrono::microseconds>(dtime).count();
-	}
-	*/
 
 	//たち下がるまで待つ
 	valueFile >> c;
